61262d6cc0ff11e9aa312ba2fdd2161442aa6ae4
Jhon Alexander <jhonh1540@gmail.com>
2025-11-11 23:04:05 -0500

    aplicacion de ecommerce UniMarket inicios

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'codecommerce_db',   # Nombre de la BD creada en phpMyAdmin
+        'USER': 'root',              # Usuario por defecto de XAMPP
+        'PASSWORD': '',              # Vacío si no has puesto contraseña en phpMyAdmin
+        'HOST': '127.0.0.1',         # O 'localhost'
+        'PORT': '3306',              # Puerto por defecto de MySQL en XAMPP
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
+
+
 
 
 # Password validation
