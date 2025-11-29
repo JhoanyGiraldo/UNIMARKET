2e050eceb38a63a24f5048cd0ff4f2dd6f6bbb45
Jhon Alexander <jhonh1540@gmail.com>
2025-11-29 08:22:26 -0500

    rama front

--- backend/myapp/tests/test_context_processors.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from django.urls import reverse
-
-@pytest.mark.django_db
-def test_contexto_global(client):
-    response = client.get(reverse('index'))
-    assert 'carrito' in response.context
--- backend/myapp/tests/test_urls.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from django.urls import reverse, resolve
-from myapp.views import catalogo
-
-def test_url_catalogo_resuelve():
-    url = reverse('catalogo')
-    match = resolve(url)
-    assert match.func == catalogo
